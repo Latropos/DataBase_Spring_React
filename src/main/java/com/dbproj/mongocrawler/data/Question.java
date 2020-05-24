5d719d207fd961cd209661f774276d677c3ffb09
fsiody <Wyzi3ros>
2020-05-24 16:13:52 +0200

    statistics ok

--- src/main/java/com/dbproj/mongocrawler/data/Question.java
+++ src/main/java/com/dbproj/mongocrawler/data/Question.java
@@ -8,7 +8,7 @@ public class Question {
     public String id;
 
     public String question;
-    public int NoAnswers;
+    public int noAnswers;
 
     public String AnswerA;
     public String AnswerB;
@@ -16,42 +16,67 @@ public class Question {
     public String AnswerD;
     public String AnswerE;
 
+    public int aCount;
+    public int bCount;
+    public int cCount;
+    public int dCount;
+    public int eCount;
+
+    public void zeroCount(){
+        this.aCount=57;
+        this.bCount=30;
+        this.cCount=2;
+        this.dCount=100;
+        this.eCount=9;
+    }
     public Question(String question, int noAnswers) {
         this.question = question;
-        this.NoAnswers = noAnswers;
+        this.noAnswers = noAnswers;
+        this.zeroCount();
+        this.countAnswers();
     }
+
 
     public Question() {
         this.question = question;
-        this.NoAnswers = 0;
+        this.noAnswers = 0;
         this.AnswerA = "";
         this.AnswerB = "";
         this.AnswerC = "";
         this.AnswerD = "";
         this.AnswerE = "";
+        this.zeroCount();
+        this.countAnswers();
     }
 
     public Question(String question, String noAnswers) {
         this.question = question;
-        this.NoAnswers = 7;
+        this.noAnswers = 5;
+        this.zeroCount();
+        this.countAnswers();
+    }
+
+    public void countAnswers(){
+        this.noAnswers=this.aCount+this.bCount+this.cCount+this.dCount+this.eCount;
     }
 
     public Question(String question, int noAnswers, String answerA, String answerB, String answerC, String answerD, String answerE) {
         this.question = question;
-        this.NoAnswers = noAnswers;
+        this.noAnswers = noAnswers;
         this.AnswerA = answerA;
         this.AnswerB = answerB;
         this.AnswerC = answerC;
         this.AnswerD = answerD;
         this.AnswerE = answerE;
+        this.zeroCount();
+        this.countAnswers();
     }
-
     @Override
     public String toString() {
         return "Question{" +
                 "id='" + id + '\'' +
                 ", question='" + question + '\'' +
-                ", NoAnswers=" + NoAnswers +
+                ", noAnswers=" + noAnswers +
                 ", AnswerA='" + AnswerA + '\'' +
                 ", AnswerB='" + AnswerB + '\'' +
                 ", AnswerC='" + AnswerC + '\'' +
--- src/main/js/components/stats.js
+++ src/main/js/components/stats.js
@@ -1,56 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import { PieChart, Pie } from "recharts";
-import { Segment, Header } from "semantic-ui-react";
-const data01 = [
+import {
+  Container,
+  Segment,
+  Form,
+  Dimmer,
+  Loader,
+  Table,
+  Button,
+  Header,
+} from "semantic-ui-react";
+import {get} from "../apiService";
+var data01 = [
   {
-    name: "A",
-    value: 400,
+    name: "AA",
+    value: 100,
   },
   {
     name: "B",
-    value: 300,
+    value: 100,
   },
   {
     name: "C",
-    value: 300,
+    value: 100,
   },
   {
     name: "D",
-    value: 200,
+    value: 100,
   },
   {
     name: "E",
-    value: 278,
-  },
-  {
-    name: "F",
-    value: 189,
-  },
-];
-const data02 = [
-  {
-    name: "A",
-    value: 2400,
-  },
-  {
-    name: "B",
-    value: 4567,
-  },
-  {
-    name: "C",
-    value: 1398,
-  },
-  {
-    name: "D",
-    value: 9800,
-  },
-  {
-    name: "E",
-    value: 3908,
-  },
-  {
-    name: "F",
-    value: 4800,
+    value: 100,
   },
 ];
 
@@ -59,35 +39,135 @@ let renderLabel = function(entry) {
 }
 
 const Stats = () => {
+  const [loading, setLoading] = useState(false);
+  console.log("STARTSTARTSATRTSATRT!!!");
+  const [results, setResults] = useState([]);
+  const getData = async () => {
+    setLoading(true);
+    let baseUrl = "/api/questions/";
+    const jsonBody = await get(baseUrl, null);
+    //setResults(jsonBody);
+    let result = [
+      {
+        name: "AA",
+        value: jsonBody[0].aCount,
+      },
+      {
+        name: "B",
+        value: jsonBody[0].bCount,
+      },
+      {
+        name: "C",
+        value: jsonBody[0].cCount,
+      },
+      {
+        name: "D",
+        value: jsonBody[0].dCount,
+      },
+      {
+        name: "E",
+        value: jsonBody[0].eCount,
+      },
+    ];
+    setResults(result);
+    setLoading(false);
+  }
+  const getStat = () => {
+    return
+  }
+  console.info(results);
   return (
-      <Segment>
-        <Header as="h3">Statistics</Header>
-        <hr />
-        <PieChart width={730} height={250}>
-          <Pie
-              data={data01}
-              dataKey="value"
-              nameKey="name"
-              cx="50%"
-              cy="50%"
-              outerRadius={50}
-              fill="#8884d8"
-          />
-          <Pie
-              data={data02}
-              dataKey="value"
-              nameKey="name"
-              cx="50%"
-              cy="50%"
-              innerRadius={60}
-              outerRadius={80}
-              fill="#82ca9d"
-              label={renderLabel}
-          />
-        </PieChart>
-      </Segment>
+      <Container>
+        <Segment>
+          <Header as="h3">Statistics</Header>
+          <hr />
+          <Button color="blue" onClick={() => getData()}>
+            Get statistics
+          </Button>
+          <PieChart width={730} height={250}>
+            <Pie
+                data={results}
+                dataKey="value"
+                nameKey="name"
+                cx="50%"
+                cy="50%"
+                outerRadius={50}
+                fill="#8884d8"
+            />
+            <Pie
+                data={results}
+                dataKey="value"
+                nameKey="name"
+                cx="50%"
+                cy="50%"
+                innerRadius={60}
+                outerRadius={80}
+                fill="#82ca9d"
+                label={renderLabel}
+            />
+          </PieChart>
+        </Segment>
+        {loading && (
+            <Dimmer active inverted>
+              <Loader inverted>Loading...</Loader>
+            </Dimmer>
+        )}
+      </Container>
   );
 };
 
 
 export default Stats;
+ /*
+ import React, {useState} from "react";
+import { PieChart, Pie } from "recharts";
+import {Segment, Header, Table, Button} from "semantic-ui-react";
+
+const [query, setQuery] = useState("");
+const [results, setResults] = useState([]);
+
+const mapResultsToData = () => {
+    //results.map((r) => (
+        data01[0].value=results.aCount;
+        data01[1].value=results.bCount;
+        data01[2].value=results.cCount;
+        data01[3].value=results.dCount;
+        data01[4].value=results.eCount;
+    //    );
+};
+
+
+let renderLabel = function(entry) {
+  return entry.name;
+}
+};
+
+
+export default Stats;
+
+ * */
+
+/*
+*
+let data01 = [
+  {
+    name: "A",
+    value: 100,
+  },
+  {
+    name: "B",
+    value: 100,
+  },
+  {
+    name: "C",
+    value: 100,
+  },
+  {
+    name: "D",
+    value: 100,
+  },
+  {
+    name: "E",
+    value: 100,
+  },
+];*/
